861ea27719746978dc779953c4e55216fe4cffbd
@anasty2234 <a.trunova@pinta.pro>
2025-04-27 15:03:38 +0300

    chore(layout): update layout component structure and improve readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+
+
+const montserrat = Montserrat({
+  weight: ["400", "700"], // Medium (400), Bold (700)
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${montserrat.variable} antialiased`}
       >
         {children}
       </body>
